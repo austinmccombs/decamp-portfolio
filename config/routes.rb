b2383f8fd10515bfd836ed2ccdb06f51aca54fa1
Austin McCombs <austinmccombs22@gmail.com>
2021-01-04 22:52:25 -0700

    fixing merge issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
-  get 'porfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolios#angular'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
-	get 'contact', to: 'pages#contact'
+  get 'contact', to: 'pages#contact'
 
-  resources :blogs do 
-  	member do 
-  		get :toggle_status
-  	end
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
   end
-
- 
- root to: 'pages#home'
+  
+  root to: 'pages#home'
 end
